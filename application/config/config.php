7627d6679f58914c0c0b9aa38863bf3c4e26d589
Sergey Bespalov <sergey.bespalov2@gmail.com>
2011-12-08 16:26:41 +0200

    add salt

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'g7slbuqpc84mboa92b6xi45kfna93kd7';
 
 /*
 |--------------------------------------------------------------------------
